f5abd1b61bc9c14e3005019e29f0bda011fde539
Grzyb3k-bit <mikolajkolodziej859@gmail.com>
2025-07-18 20:22:20 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/move-page-cleangit remote add origin https://github.com/Grzyb3k-bit/move-page-clean.git",
+  base: "/move-page-clean",
   server: {
     historyApiFallback: true,
   },
